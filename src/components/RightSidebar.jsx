9f7022f98c06ed512a8768c21183b99442ab0693
janidu98 <janinduchathushan@gmail.com>
2025-07-02 21:02:03 +0530

    Completed home and rightsidebar UI

--- src/components/RightSidebar.jsx
+++ src/components/RightSidebar.jsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import React from "react";
+import assets, { imagesDummyData } from "../assets/assets";
 
-const RightSidebar = () => {
+const RightSidebar = ({ selectedUser }) => {
   return (
-    <div>
-      <h1>RightSidebar</h1>
-    </div>
-  )
-}
+    selectedUser && (
+      <div
+        className={`bg-[#8185B2]/10 text-white w-full relative overflow-y-scroll ${
+          selectedUser ? "max-md:hidden" : ""
+        }`}
+      >
+        <div className="pt-16 flex flex-col items-center gap-2 font-light text-xs mx-auto">
+          <img
+            src={selectedUser?.profilePic || assets.avatar_icon}
+            alt="profile_pic"
+            className="w-20 aspect-[1/1] rounded-full"
+          />
+          <h1 className="px-10 text-lg mx-auto font-medium flex items-center gap-2">
+            <p className="h-2 w-2 rounded-full bg-green-500"></p>
+            {selectedUser.fullName}
+          </h1>
+          <p className="px-10 mx-auto">{selectedUser.bio}</p>
+        </div>
 
-export default RightSidebar
+        <hr className="bg-[#ffffff50 my-4 mx-5" />
+
+        <div className="px-5 text-xs">
+          <p>Media</p>
+          <div className="mt-2 max-h-[200px] overflow-y-scroll grid grid-cols-2 gap-4 opacity-80">
+            {imagesDummyData.map((url, index) => (
+              <div
+                key={index}
+                className="cursor-pointer rounded"
+                onClick={() => window.open(url)}
+              >
+                <img src={url} alt="" className="h-full rounded-md" />
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <button className="absolute bottom-5 left-1/2 transform -translate-x-1/2 bg-gradient-to-r from-purple-400 to-violet-600 text-white border-none font-light text-sm py-2 px-20 rounded-full cursor-pointer">
+          Logout
+        </button>
+      </div>
+    )
+  );
+};
+
+export default RightSidebar;
